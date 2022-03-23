41e2969e7e63d20f8652e41ddbe22df53d921ce7
Ryan Gorman <ryangorman@nevada.unr.edu>
2022-03-23 03:09:38 -0700

    Add project files.

---
+++ Intermediate/Build/Win64/OpenCVModuleEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30706
+#include "F:/Documents/Unreal Projects/OpenCVModule/Intermediate/Build/Win64/OpenCVModuleEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/OpenCVModule/OpenCVModuleGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef OPENCVMODULE_OpenCVModuleGameModeBase_generated_h
+#error "OpenCVModuleGameModeBase.generated.h already included, missing '#pragma once' in OpenCVModuleGameModeBase.h"
+#endif
+#define OPENCVMODULE_OpenCVModuleGameModeBase_generated_h
+
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_SPARSE_DATA
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_RPC_WRAPPERS
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAOpenCVModuleGameModeBase(); \
+	friend struct Z_Construct_UClass_AOpenCVModuleGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AOpenCVModuleGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/OpenCVModule"), NO_API) \
+	DECLARE_SERIALIZER(AOpenCVModuleGameModeBase)
+
+
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAOpenCVModuleGameModeBase(); \
+	friend struct Z_Construct_UClass_AOpenCVModuleGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AOpenCVModuleGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/OpenCVModule"), NO_API) \
+	DECLARE_SERIALIZER(AOpenCVModuleGameModeBase)
+
+
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOpenCVModuleGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOpenCVModuleGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOpenCVModuleGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOpenCVModuleGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOpenCVModuleGameModeBase(AOpenCVModuleGameModeBase&&); \
+	NO_API AOpenCVModuleGameModeBase(const AOpenCVModuleGameModeBase&); \
+public:
+
+
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOpenCVModuleGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOpenCVModuleGameModeBase(AOpenCVModuleGameModeBase&&); \
+	NO_API AOpenCVModuleGameModeBase(const AOpenCVModuleGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOpenCVModuleGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOpenCVModuleGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOpenCVModuleGameModeBase)
+
+
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_12_PROLOG
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_SPARSE_DATA \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_RPC_WRAPPERS \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_INCLASS \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_SPARSE_DATA \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> OPENCVMODULE_API UClass* StaticClass<class AOpenCVModuleGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID OpenCVModule_Source_OpenCVModule_OpenCVModuleGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
